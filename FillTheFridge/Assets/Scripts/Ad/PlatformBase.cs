9402088834b644b52bb83b4f09bd2644768515ca
DESKTOP-AFQV2GT\Admin <YuDong>
2025-05-29 18:43:15 +0800

    回滚提交

--- FillTheFridge/Assets/Scripts/Ad/PlatformBase.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PlatformBase 
-{
-    public bool IsInit = false;
-  public  abstract void Init();
-}
